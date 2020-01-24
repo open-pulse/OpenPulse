a532cbe0c7d93dfab02602af777294295f444061
Andre Fernandes <fpf.andre@gmail.com>
2020-01-24 12:48:37 -0300

    split_sequence added to util function

---
+++ pulse/utils.py
@@ -0,0 +1,7 @@
+def split_sequence(sequence, size):
+    subsequences = []
+    for start in range(0, len(sequence), size):
+        end = start + size
+        subsequence = sequence[start:end]
+        subsequences.append(subsequence)
+    return subsequences
