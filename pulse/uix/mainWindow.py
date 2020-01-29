3ae5b669b5cbad3a6574c05b306d906a280d8057
Andre Fernandes <fpf.andre@gmail.com>
2020-01-29 11:40:28 -0300

    separed action creation in a funcion. Trying to make window resizable.

--- pulse/uix/mainWindow.py
+++ pulse/uix/mainWindow.py
@@ -1,13 +1,11 @@
 import sys
-import os
+from os.path import expanduser
 from pathlib import Path
 import vtk
-from os.path import expanduser
-from PyQt5 import QtCore, QtGui
-from PyQt5 import Qt
 import numpy as np
+from PyQt5.QtCore import Qt
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QWidget, QPushButton, QAction, QDirModel, QTreeView, QToolBar
+from PyQt5.QtWidgets import QMainWindow, QWidget, QPushButton, QAction, QDirModel, QTreeView, QToolBar, QSplitter
 from PyQt5.QtCore import QSize
 from PyQt5.QtGui import QIcon, QStandardItemModel, QStandardItem
 
@@ -17,10 +15,10 @@ from pulse.opv.openPulse3DLines import OpenPulse3DLines
 
 from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 
-class MainWindow(Qt.QMainWindow):
+class MainWindow(QMainWindow):
 
     def __init__(self, parent = None):
-        Qt.QMainWindow.__init__(self, parent)
+        QMainWindow.__init__(self, parent)
         self.init()
         self.show()
 
@@ -29,6 +27,7 @@ class MainWindow(Qt.QMainWindow):
         icons_path = self.getIconPath()
         self.setWindowIcon(QIcon(icons_path+'new.png'))
         self.changeWindowTitle()
+        self.create_actions()
         self.createMenuBar()
         self.createToolBar()
         self.createBasicLayout()
@@ -39,33 +38,33 @@ class MainWindow(Qt.QMainWindow):
             title += " - " + msg
         self.setWindowTitle(title)
 
-    def createMenuBar(self):
-
+    def create_actions(self):
         icons_path = self.getIconPath()
 
-        newAction = QAction(QIcon(icons_path + 'new.png'), '&New', self)        
-        newAction.setShortcut('Ctrl+N')
-        newAction.setStatusTip('New document')
-        newAction.triggered.connect(self.newCall)
+        self.new_action = QAction(QIcon(icons_path + 'new.png'), '&New', self)        
+        self.new_action.setShortcut('Ctrl+N')
+        self.new_action.setStatusTip('New document')
+        self.new_action.triggered.connect(self.newCall)
 
-        openAction = QAction(QIcon(icons_path + 'open.png'), '&Import Geometry', self)        
-        openAction.setShortcut('Ctrl+O')
-        openAction.setStatusTip('Import Geometry')
-        openAction.triggered.connect(self.importCall)
+        self.import_action = QAction(QIcon(icons_path + 'open.png'), '&Import Geometry', self)        
+        self.import_action.setShortcut('Ctrl+O')
+        self.import_action.setStatusTip('Import Geometry')
+        self.import_action.triggered.connect(self.importCall)
 
-        exitAction = QAction(QIcon(icons_path + 'exit.png'), '&Exit', self)        
-        exitAction.setShortcut('Ctrl+Q')
-        exitAction.setStatusTip('Exit application')
-        exitAction.triggered.connect(self.close)
+        self.exit_action = QAction(QIcon(icons_path + 'exit.png'), '&Exit', self)        
+        self.exit_action.setShortcut('Ctrl+Q')
+        self.exit_action.setStatusTip('Exit application')
+        self.exit_action.triggered.connect(self.close)
 
+    def createMenuBar(self):
         menuBar = self.menuBar()
 
         fileMenu = menuBar.addMenu('&File')
         helpMenu = menuBar.addMenu("&Help")
 
-        fileMenu.addAction(newAction)
-        fileMenu.addAction(openAction)
-        fileMenu.addAction(exitAction)
+        fileMenu.addAction(self.new_action)
+        fileMenu.addAction(self.import_action)
+        fileMenu.addAction(self.exit_action)
 
     def createToolBar(self):
         self.toolbar = QToolBar("Enable Toolbar")
@@ -73,32 +72,28 @@ class MainWindow(Qt.QMainWindow):
         self.toolbar.setMovable(False)
         self.addToolBar(self.toolbar)
 
-        icons_path = self.getIconPath()
-
-        newAction = QAction(QIcon(icons_path + 'new.png'), '&New', self)        
-        newAction.setShortcut('Ctrl+N')
-        newAction.setStatusTip('New document')
-        newAction.triggered.connect(self.newCall)
-
-        openAction = QAction(QIcon(icons_path + 'open.png'), '&Import Geometry', self)        
-        openAction.setShortcut('Ctrl+O')
-        openAction.setStatusTip('Import Geometry')
-        openAction.triggered.connect(self.importCall)
-
-        exitAction = QAction(QIcon(icons_path + 'exit.png'), '&Exit', self)        
-        exitAction.setShortcut('Ctrl+Q')
-        exitAction.setStatusTip('Exit application')
-        exitAction.triggered.connect(self.close)
-
-        self.toolbar.addAction(newAction)
-        self.toolbar.addAction(openAction)
-        
-        
+        self.toolbar.addAction(self.new_action)
+        self.toolbar.addAction(self.import_action)      
         self.toolbar.addSeparator()
 
     def createBasicLayout(self):
-        generalFrame = Qt.QFrame()
-        generalLayout = Qt.QHBoxLayout()
+        info_widget = QWidget()
+        info_widget.setStyleSheet('background-color:red')
+
+        graphics_widget = QWidget()
+        graphics_widget.setStyleSheet('background-color:green')
+
+        working_area_splitter = QSplitter(Qt.Horizontal)
+        working_area_splitter.addWidget(info_widget)
+        working_area_splitter.addWidget(graphics_widget)
+        working_area_splitter.setSizes([70,100])
+
+        self.setCentralWidget(working_area_splitter)
+
+        return 
+
+        generalFrame = QFrame()
+        generalLayout = QHBoxLayout()
 
         self.infoLayout = InfoLayout(self)
         self.OPVLayout = OPVLayer(self)
