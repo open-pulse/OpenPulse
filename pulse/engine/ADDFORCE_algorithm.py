8d11c72d1c08569c85279002e393c7d3c208a7a2
olavosilva <olavus@gmail.com>
2020-02-06 10:58:01 -0200

    Basic algorithms for Reaction and Equiv. Forces

--- pulse/engine/ADDFORCE_algorithm.py
+++ pulse/engine/ADDFORCE_algorithm.py
@@ -3,7 +3,11 @@ import math
 import numpy as np
 """
 General algorithm for obtaining the additional load vector 'Fadd' when some degrees of
-freedom 'R' are prescribed with zero or non-zero values 'V'.
+freedom 'R' are prescribed with zero or non-zero values, 'V'.
+
+Matrices and vectors randomly produced to build the algorithm.
+
+THE HARMONIC PROBLEM HAS NOT YET BEEN SOLVED.
 
 """
 ####################### 2-node 6-DOF/node ###########################
@@ -12,13 +16,19 @@ freedom 'R' are prescribed with zero or non-zero values 'V'.
 freq = 100 # Hz   #OBS.: THE ADDITIONAL LOAD VECTOR IS DEPENDENT ON THE FREQUENCY!
 omega = 2.*np.pi*freq
 #
+#############
+#
 totaldof = 60
 prescdof = 6
 freedof = totaldof - prescdof
-# Array with Prescribed Degrees of Freedom (Global, internal):
-R = np.array([1, 15, 19, 20, 24, 59 ])  #6 prescribed dofs
+# Array with ALL Degrees of Freedom (Global, internal):
+DOFS = np.arange(60) + 1
+# Array with PRESCRIBED Degrees of Freedom (Global, internal):
+R = np.array([1, 15, 19, 20, 24, 59 ])
+# Array with FREE Degrees of Freedom (Global, internal):
+D = np.delete(DOFS, [R-1] , None)
 # Array with Values for Prescribed Degrees of Freedom:
-V = np.array([0.01, 0., 0.002, -0.1, 0.25, 0.01])  #all prescribed dofs, even if zero valued dofs (constraints)
+V = np.array([0.01, 0., 0.002, -0.1, 0.25, 0.01])
 #
 # Global matrices eliminating prescribed dofs:
 K = np.random.rand(54,54)
@@ -26,9 +36,9 @@ M = np.random.rand(54,54)
 C = np.random.rand(54,54) #if damped problem
 #
 # Matrices of eliminated lines:
-KR = np.random.rand(6,54)
-MR = np.random.rand(6,54)
-CR = np.random.rand(6,54) # if damped problem
+KR = np.random.rand(6,60)
+MR = np.random.rand(6,60)
+CR = np.random.rand(6,60) # if damped problem
 #
 Fadd = np.zeros((freedof))
 FKadd = np.zeros((freedof))
@@ -36,11 +46,12 @@ FMadd = np.zeros((freedof))
 FCadd = np.zeros((freedof))
 # Additional load vector applied to free dofs
 for i in range(freedof):  # 'i' runs over the free degrees of freedom
-    FKadd[i] = FKadd[i] + KR[:,i].T @ V
-    FMadd[i] = FMadd[i] + MR[:,i].T @ V
-    FCadd[i] = FCadd[i] + CR[:,i].T @ V
+    j = D[i]-1
+    FKadd[i] = FKadd[i] + KR[:,j].T @ V
+    FMadd[i] = FMadd[i] + MR[:,j].T @ V
+    FCadd[i] = FCadd[i] + CR[:,j].T @ V
 #
 Fadd = FKadd - (omega**2.0)*FMadd + omega*FCadd*1j
 #
-#OBS.: The matrices of eliminated lines 'KR', 'MR' and 'CR' will be used in the postprocessing routine to
-#      calculate the reaction forces!!!
+#OBS.: The matrices of eliminated lines 'KR', 'MR' and 'CR' will be used in ]
+#      the postprocessing routines to calculate the reaction forces!!!
--- pulse/engine/REACTFORCE_algorithm.py
+++ pulse/engine/REACTFORCE_algorithm.py
@@ -2,44 +2,69 @@
 import math
 import numpy as np
 """
-General algorithm for obtaining the reaction forces 'Freact' on the constrained nodes.
+General algorithm for obtaining the reaction forces 'Freact' when some degrees of
+freedom 'R' are prescribed with zero or non-zero values, 'V'.
 THIS IS A POST-PROCESSING PROCEDURE. It is assumed that the solution U is already obtained.
+
+Matrices and vectors randomly produced to build the algorithm.
+
+THE HARMONIC PROBLEM WAS SOLVED AND WE HAVE THE SOLUTION.
+
 """
 ####################### 2-node 6-DOF/node ###########################
 # Total Degrees of Freedom = 60 (10 nodes)
 #
-freq = 100 # Hz   #OBS.: THE ADDITIONAL LOAD VECTOR IS DEPENDENT ON THE FREQUENCY!
+freq = 100 # Hz   #OBS.: THE REACTION FORCES ARE DEPENDENT ON THE FREQUENCY!
 omega = 2.*np.pi*freq
 #
-totaldof = 60
-prescdof = 5
-freedof = totaldof - prescdof
-# Array with Prescribed Degrees of Freedom (Global, internal):
-R = np.array([1, 19, 20, 24, 59 ])
-# Array with Values for Prescribed Degrees of Freedom:
-V = np.array([0.01, 0.002, -0.1, 0.25, 0.01])
+#############
 #
+totaldof = 60
+prescdof = 6
+freedof = totaldof - prescdof
+# Array with ALL Degrees of Freedom (Global, internal):
+DOFS = np.arange(60) + 1
+# Array with PRESCRIBED Degrees of Freedom (Global, internal):
+R = np.array([1, 15, 19, 20, 24, 59 ])
+# Array with FREE Degrees of Freedom (Global, internal):
+D = np.delete(DOFS, [R-1] , None)
+# Array with Values for Prescribed Degrees of Freedom:
+V = np.array([0.01, 0., 0.002, -0.1, 0.25, 0.01])
+#
+#############
 # Global matrices eliminating constrained dofs:
-K = np.random.rand(55,55)
-M = np.random.rand(55,55)
-C = np.random.rand(55,55) #if damped problem
+K = np.random.rand(54,54)
+M = np.random.rand(54,54)
+C = np.random.rand(54,54) #if damped problem
 #
 # Matrices of eliminated lines:
-KR = np.random.rand(5,55)
-MR = np.random.rand(5,55)
-CR = np.random.rand(5,55) # if damped problem
+KR = np.random.rand(6,60)
+MR = np.random.rand(6,60)
+CR = np.random.rand(6,60) # if damped problem
 #
-Fadd = np.zeros((freedof))
-FKadd = np.zeros((freedof))
-FMadd = np.zeros((freedof))
-FCadd = np.zeros((freedof))
+# Here, the vector U_T_omega is obtained from R and D. However, the code
+# must be implemented to "deliver" U_T_omega.
+#
+# Solution of harmonic problem:
+U_omega = np.random.rand(freedof)  #U(omega)  --- U IS DEPENDENT ON OMEGA!
+# Global U_T vector, with all degrees of freedom, including constrained ones
+U_T_omega = np.zeros(totaldof)
+for i in range(freedof):
+    U_T_omega[D[i]-1]=U_omega[i]
+for i in range(prescdof):
+    U_T_omega[R[i]-1]=V[i]
+#
+Freact = np.zeros((prescdof))
+FKreact = np.zeros((prescdof))
+FMreact = np.zeros((prescdof))
+FCreact = np.zeros((prescdof))
 # Additional load vector applied to free dofs
-for i in range(freedof):  # 'i' runs over the free degrees of freedom
-    FKadd[i] = FKadd[i] + KR[:,i].T @ V
-    FMadd[i] = FMadd[i] + MR[:,i].T @ V
-    FCadd[i] = FCadd[i] + CR[:,i].T @ V
+for i in range(prescdof):  # 'i' runs over the prescribed degrees of freedom
+    FKreact[i] = FKreact[i] + KR[i,:] @ U_T_omega
+    FMreact[i] = FMreact[i] + MR[i,:] @ U_T_omega
+    FCreact[i] = FCreact[i] + CR[i,:] @ U_T_omega
 #
-Fadd = FKadd - (omega**2.0)*FMadd + omega*FCadd*1j
+Freact = FKreact - (omega**2.0)*FMreact + omega*FCreact*1j
 #
-#OBS.: The matrices of eliminated lines 'KR', 'MR' and 'CR' will be used in the postprocessing routine to
-#      calculate the reaction forces!!!
+#OBS.: The matrices of eliminated lines 'KR', 'MR' and 'CR' will be used in 
+#      the postprocessing routines in order to calculate the reaction forces!!!
