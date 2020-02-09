1984c7f97409a04fd6f2f48fdbe762614cce9add
JacsonGilVargas <jgvargasemc@gmail.com>
2020-02-09 01:19:46 -0300

    Assemble updating

--- pulse/engine/assembly.py
+++ pulse/engine/assembly.py
@@ -223,7 +223,7 @@ class Assembly:
             #     count += aux
         
             # for i, line_dof in enumerate(local_dof):
-            I_aux = global_dof.reshape(12,1)*np.ones((1,12))
+            I_aux = global_dof.reshape(global_dof.shape[0],1)*np.ones((1,global_dof.shape[0]))
             J_aux = I_aux.T
             I[count : count + aux]  = I_aux.reshape(entries_per_element)
             J[count : count + aux]  = J_aux.reshape(entries_per_element)
@@ -232,11 +232,6 @@ class Assembly:
 
             count += aux
         
-
-
-
-
-
         # Line and Collumn Elimination
         
         global_dofs_presc = np.sort( self.dofs_fixed() )
