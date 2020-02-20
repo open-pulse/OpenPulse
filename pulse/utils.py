f5552248b8186150cab3293880850d7b33f6aa95
Andre Fernandes <fpf.andre@gmail.com>
2020-02-20 17:50:49 -0300

    new convertion functions added to utils

--- pulse/utils.py
+++ pulse/utils.py
@@ -5,3 +5,9 @@ def split_sequence(sequence, size):
         subsequence = sequence[start:end]
         subsequences.append(subsequence)
     return subsequences
+
+def m_to_mm(m):
+    return float(m) * 1000
+
+def mm_to_m(mm):
+    return float(mm) / 1000
