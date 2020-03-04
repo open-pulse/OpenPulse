7b87e856865fb1434b89e7be90bbe986142a8a67
Andre Fernandes <fpf.andre@gmail.com>
2020-03-04 13:20:27 -0300

    documentate utils module

--- pulse/utils.py
+++ pulse/utils.py
@@ -1,7 +1,39 @@
+'''
+utils
+=====
+
+Utils module provides a bunch of general utility functions and classes 
+that may be usefull for some applications.
+
+'''
+
+
 from functools import wraps
 from time import time
 
 def split_sequence(sequence, size):
+    '''
+    Function used to divide a sequence in a list of sequences with given size.
+
+
+    PARAMETERS
+    ----------
+    sequence : list, tuple, np.ndarray, and other array like objects
+    size: int
+
+
+    RETURNS
+    -------
+    subsequences : list
+
+
+    EXAMPLE
+    --------
+    >>> sequence = [1, 2, 3, 4, 5, 6]
+    >>> split_sequence(sequence, 2)
+    [[1,2], [3,4], [5,6]]
+    '''
+
     subsequences = []
     for start in range(0, len(sequence), size):
         end = start + size
@@ -11,6 +43,37 @@ def split_sequence(sequence, size):
 
 
 def slicer(iterable, argument):
+    '''
+    Function used to create a custom slice of an iterable.
+
+    PARAMETERS
+    ----------
+    iterable : any iterable object
+    argument : can be a list of indexes, integer index, or string 'all'
+
+
+    YELDS
+    -----
+    value of iterable in given indexes
+
+
+    EXAMPLE
+    -------
+    >>> sequence = ['a', 'b', 'c', 'd']
+    >>> for i in slicer(sequence, [1,2]):
+    ...     print(i)
+    ... 
+    'b'
+    'c'
+    >>> for i in slicer(sequence, 'all'):
+    ...     print(i)
+    ... 
+    'a'
+    'b'
+    'c'
+    'd'
+    '''
+
     if isinstance(argument, str) and argument == 'all':
         if isinstance(iterable, dict):
             for i in iterable.values():
@@ -31,6 +94,20 @@ def slicer(iterable, argument):
 
 
 def timer(function):
+    '''
+    Decorator to calculate the time taken for a function to run.
+    
+
+    EXAMPLE
+    -------
+    >>> @timer
+    ... def foo():
+    ...     print('foo')
+    ...
+    >>> foo()
+    foo
+    Time to finish foo: 0.0 [s]
+    '''
     @wraps(function)
     def wrapper(*args, **kwargs):
         start_time = time()
@@ -42,7 +119,44 @@ def timer(function):
     
 
 def m_to_mm(m):
+    '''
+    Simple function to convert meters to milimeters.
+    
+
+    PARAMETERS
+    ----------
+    m : int, float
+
+
+    RETURNS
+    -------
+    mm : float
+
+    EXAMPLE
+    -------
+    >>> m_to_mm(7)
+    7000
+    '''
     return float(m) * 1000
 
 def mm_to_m(mm):
+    '''
+    Simple function to convert meters to milimeters.
+    
+
+    PARAMETERS
+    ----------
+    mm : int, float
+
+
+    RETURNS
+    -------
+    m : float
+
+
+    EXAMPLE
+    -------
+    >>> mm_to_m(7000)
+    7
+    '''
     return float(mm) / 1000
