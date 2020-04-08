f61412d7763dd0a75edc74642e5a2c0d8cb37422
Andre Fernandes <fpf.andre@gmail.com>
2020-04-08 12:35:22 -0300

    get normal created

--- pulse/postprocessing/plot_data.py
+++ pulse/postprocessing/plot_data.py
@@ -8,16 +8,16 @@ def get_frf(mesh, solution, node, dof):
     y = np.abs(solution[position])
     return y
 
-def get_normal(mesh, solution, node):
+def get_normal(mesh, solution, node, frequency):
     start_node = mesh.nodes[node]
     position = start_node.global_index * DOF_PER_NODE
-    x = solution[position + 0]
-    y = solution[position + 1]
-    z = solution[position + 2]
+    x = solution[position + 0][frequency]
+    y = solution[position + 1][frequency]
+    z = solution[position + 2][frequency]
 
-    dx = (x + start_node.x)
-    dy = (y + start_node.y)
-    dz = (z + start_node.z)
+    dx = (x - start_node.x)
+    dy = (y - start_node.y)
+    dz = (z - start_node.z)
 
     normal = np.sqrt(dx*dx + dy*dy + dz*dz)
 
