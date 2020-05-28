8c4564655cc6c10dcf00cbc768e8902c0a8d4468
olavosilva <olavus@gmail.com>
2020-05-28 14:46:43 -0300

    Stress - SV

--- pulse/engine/stress.py
+++ pulse/engine/stress.py
@@ -10,7 +10,7 @@ hyst = 0.
 #exemplo: 3 elementos, 4 nos
 nel=3
 nnode=4
-connect = np.array([1, 1, 2],[2, 2, 3],[3, 3, 4])
+connect = np.array([[1, 1, 2],[2, 2, 3],[3, 3, 4]])
 U = np.random.rand(24)
 P = np.random.rand(4)
 
@@ -32,7 +32,6 @@ Calculation at the external surface.
     p0 = 0.
     Ke = np.random.rand(12,12)
     Me = np.random.rand(12,12)
-    connect = np.array([1, 1, 2],[2, 2, 3],[3, 3, 4])
     #
     # Calculando: ###################################
     w = 2.0*np.pi*freq
@@ -44,11 +43,11 @@ Calculation at the external surface.
     n2 = connect[el-1,2]
     Ue[0:6] = U[(6*n1-5)-1:(6*n1)] 
     Ue[6:12] = U[(6*n2-5)-1:(6*n2)]
-    Pe[0] = P[n1]
-    Pe[1] = P[n2]
+    Pe[0] = P[n1-1]
+    Pe[1] = P[n2-1]
     Pm = (Pe[0] + Pe[1])/2
     #
-    Sh = (2*Pm*(di^2) - p0*(do^2 + di^2))/(do^2 - di^2) 
+    Sh = (2*Pm*(di**2) - p0*(do**2 + di**2))/(do**2 - di**2) 
     #
     Fe = KD @ Ue
     # Fe = [FX1, FY1, FZ1, MX1, MY1, MZ1, FX2, FY2, FZ2, MX2, MY2, MZ2]
@@ -61,18 +60,18 @@ Calculation at the external surface.
 
     return SX1, SS1, SX2, SS2
     #
-SVEC = np.zeros(nnode,2)
+SVEC = np.zeros((nnode,2))
 a = np.zeros(nnode)
 for el in range(nel):
     SX1, SS1, SX2, SS2 = stressel(el,connect,freq,alpha,beta,hyst,U,P)
     #
     n1 = connect[el-1,1]
-    a[n1] = 1 #Aqui o 'a' seria um contado de quantas vezes o "nó 1" é chamado! Teria que implementar! Para a média nodal.
+    a[n1-1] = 1 #IMPLEMENTAR: Aqui o 'a' seria um vetor. Vai armazendo quantas vezes cada nó é chamado! Para a média nodal. Deixei 1 de bobeira.
     n2 = connect[el-1,2]
-    a[n2] = 1 #Aqui o 'a' seria um contado de quantas vezes o "nó 2" é chamado! Teria que implementar! Para a média nodal.
+    a[n2-1] = 1 #IMPLEMENTAR: Aqui o 'a' seria um vetor. Vai armazendo quantas vezes cada nó é chamado! Para a média nodal. Deixei 1 de bobeira.
     #
-    SVEC[n1,0:2] = SVEC[n1,0:2] + np.array([SX1, SS1])
-    SVEC[n2,0:2] = SVEC[n1,0:2] + np.array([SX2, SS2])
+    SVEC[n1-1,0:2] = SVEC[n1,0:2] + np.array([SX1, SS1])
+    SVEC[n2-1,0:2] = SVEC[n1,0:2] + np.array([SX2, SS2])
 for i in range(nel):
     SVEC[i,:]=SVEC[i,:]/a[i]
     #
