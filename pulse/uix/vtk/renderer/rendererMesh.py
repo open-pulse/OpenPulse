2462a0e235d604c1c3978d6cd242f7e3257d4746
andrefpf <fpf.andre@gmail.com>
2020-08-04 19:46:48 -0300

    creating axes on boundary condition

--- pulse/uix/vtk/renderer/rendererMesh.py
+++ pulse/uix/vtk/renderer/rendererMesh.py
@@ -11,8 +11,9 @@ class RendererMesh(vtkRendererBase):
         self.project = project
         self.opv = opv 
 
-        self.pointsID = {}
-        self.elementsID = {}
+        self.pointsID = dict()
+        self.elementsID = dict()
+        self.axes = dict()
 
         self._rendererPoints = vtk.vtkRenderer()
         self._rendererElements = vtk.vtkRenderer()
@@ -135,7 +136,8 @@ class RendererMesh(vtkRendererBase):
         for key, element in self.project.get_elements().items():
             plot = ActorElement(element, 0.01, key)
             plot.build()
-            source.AddInputData(plot.getActor().GetMapper().GetInput())
+            actor = plot.getActor()
+            source.AddInputData(actor.GetMapper().GetInput())
         
         mapper.SetInputConnection(source.GetOutputPort())
         actor.SetMapper(mapper)
@@ -165,3 +167,257 @@ class RendererMesh(vtkRendererBase):
             actor = plot.getActor()
             self.elementsID[actor] = key
             self._rendererElements.AddActor(actor)
+
+
+
+
+
+###
+    def getSize(self):
+        return 0
+    
+    def transformPoints(self, points_id):
+        self._style.clear()
+        for ID in points_id:
+            node = self.project.get_node(ID)
+            self.plotAxes(node, ID)
+            # try:
+            # except Exception as e:
+            #     print(e)
+    
+    def plotAxes(self, node, key_id):
+        self.removeAxes(key_id)
+        self.axes[key_id] = []
+        self.plotForceAxes(node, key_id)
+        self.plotBcAxes(node, key_id)
+        self.plotRotationAxes(node, key_id)
+        self.plotMomentoAxes(node, key_id)
+        self.updateInfoText()
+
+    def removeAxes(self, key):
+        if self.axes.get(key) is not None:
+            for actor in self.axes[key]:
+                self._renderer.RemoveActor(actor)
+            self.axes.pop(key)
+
+    def transformAxe(self, node, negative_axe):
+        transform = vtk.vtkTransform()
+        transform.Translate(node.x, node.y, node.z)
+        if negative_axe[0] == 1:
+            transform.RotateZ(90)
+            if negative_axe[1] == 1:
+                transform.RotateY(270)
+                if negative_axe[2] == 1:
+                    transform.RotateY(-90)
+                #Z +
+            else:
+                #Y +
+                if negative_axe[2] == 1:
+                    transform.RotateY(90)
+        else:
+            #X +
+            if negative_axe[1] == 1:
+                transform.RotateX(180)
+                transform.RotateY(90)
+                if negative_axe[2] == 1:
+                    transform.RotateZ(90)
+                #Z +
+            else:
+                #Y +
+                if negative_axe[2] == 1:
+                    transform.RotateZ(90)
+                    transform.RotateX(180)
+        return transform
+
+    def getPosAxe(self, negative_axe, have_axe):
+        if negative_axe[0] == 1:
+            pos_axes = [1,0,2]
+            if negative_axe[1] == 1:
+                if negative_axe[2] == 1:
+                    pos_axes = [1,0,2]
+                else:
+                    pos_axes = [1,2,0]
+            else:
+                if negative_axe[2] == 1:
+                    pos_axes = [1,2,0]
+                else:
+                    pos_axes = [1,0,2]
+        else:
+            pos_axes = [0,1,2]
+            if negative_axe[1] == 1:
+                if negative_axe[2] == 1:
+                    pos_axes = [2,0,1]
+                else:
+                    pos_axes = [2,1,0]
+            else:
+                if negative_axe[2] == 1:
+                    pos_axes = [1,0,2]
+                else:
+                    pos_axes = [0,1,2]
+
+        temp = [0,0,0]
+        temp[pos_axes[0]] = have_axe[0]
+        temp[pos_axes[1]] = have_axe[1]
+        temp[pos_axes[2]] = have_axe[2]
+        return temp
+    
+    def getReal(self, vector):
+        new_vector = vector.copy()
+        for i in range(len(vector)):
+            if type(vector[i]) == complex:
+                new_vector[i] = vector[i].real
+        return new_vector
+
+    def plotBcAxes(self, node, key_id):
+        bc = self.getReal(node.getStructuralBondaryCondition())
+        have_bc = [0,0,0]
+        negative_bc = [0,0,0]
+        for i in range(0,3):
+            if bc[i] is not None:
+                have_bc[i] = 1
+            if bc[i] is not None and bc[i] < 0:
+                negative_bc[i] = 1
+
+        if have_bc.count(0) == 3:
+            return
+        
+        transform = self.transformAxe(node, negative_bc)
+        have_bc = self.getPosAxe(negative_bc, have_bc)
+        
+        axe = vtk.vtkAxesActor()
+        axe.AxisLabelsOff()
+        axe.SetTotalLength(0.02*have_bc[0],0.02*have_bc[1],0.02*have_bc[2])
+        axe.SetShaftTypeToLine()
+        axe.SetNormalizedTipLength(0.8*have_bc[0],0.8*have_bc[1],0.8*have_bc[2])
+        axe.SetUserTransform(transform)
+        axe.GetXAxisShaftProperty().SetColor(0,1,0)
+        axe.GetYAxisShaftProperty().SetColor(0,1,0)
+        axe.GetZAxisShaftProperty().SetColor(0,1,0)
+
+        axe.GetXAxisTipProperty().SetColor(0,1,0)
+        axe.GetYAxisTipProperty().SetColor(0,1,0)
+        axe.GetZAxisTipProperty().SetColor(0,1,0)
+        self.axes[key_id].append(axe)
+        self._renderer.AddActor(axe)
+
+    def plotRotationAxes(self, node, key_id):
+        rotation = self.getReal(node.getStructuralBondaryCondition())
+        have_rotation = [0,0,0]
+        negative_rotation = [0,0,0]
+        for i in range(3,6):
+            if rotation[i] is not None and rotation[i] != 0:
+                have_rotation[i-3] = 1
+            if rotation[i] is not None and rotation[i] < 0:
+                negative_rotation[i-3] = 1
+
+        if have_rotation.count(0) == 3:
+            return
+        
+        transform = self.transformAxe(node, negative_rotation)
+        have_rotation = self.getPosAxe(negative_rotation, have_rotation)
+        
+        axe = vtk.vtkAxesActor()
+        axe.AxisLabelsOff()
+        axe.SetTotalLength(0.03*have_rotation[0],0.03*have_rotation[1],0.03*have_rotation[2])
+        axe.SetShaftTypeToLine()
+        axe.SetNormalizedTipLength(0.5*have_rotation[0],0.5*have_rotation[1],0.5*have_rotation[2])
+        axe.SetUserTransform(transform)
+        axe.GetXAxisShaftProperty().SetColor(1,0.64,0)
+        axe.GetYAxisShaftProperty().SetColor(1,0.64,0)
+        axe.GetZAxisShaftProperty().SetColor(1,0.64,0)
+
+        axe.GetXAxisTipProperty().SetColor(1,0.64,0)
+        axe.GetYAxisTipProperty().SetColor(1,0.64,0)
+        axe.GetZAxisTipProperty().SetColor(1,0.64,0)
+        self.axes[key_id].append(axe)
+        self._renderer.AddActor(axe)
+
+    def plotMomentoAxes(self, node, key_id):
+        momento = self.getReal(node.get_prescribed_loads())
+        have_momento = [0,0,0]
+        negative_momento = [0,0,0]
+        for i in range(3,6):
+            if momento[i] is not None and momento[i] != 0:
+                have_momento[i-3] = 1
+            if momento[i] is not None and momento[i] < 0:
+                negative_momento[i-3] = 1
+
+        if have_momento.count(0) == 3:
+            return
+
+        transform = self.transformAxe(node, negative_momento)
+        have_momento = self.getPosAxe(negative_momento, have_momento)
+        
+        axe = vtk.vtkAxesActor()
+        axe.AxisLabelsOff()
+        axe.SetTotalLength(0.15*have_momento[0], 0.15*have_momento[1], 0.15*have_momento[2])
+        axe.SetShaftTypeToLine()
+        axe.SetShaftTypeToCylinder()
+        axe.SetCylinderRadius(self.getSize()*2)
+        axe.SetNormalizedTipLength(0.2*have_momento[0], 0.2*have_momento[1], 0.2*have_momento[2])
+        axe.SetUserTransform(transform)
+
+        axe.GetXAxisShaftProperty().SetColor(0,0,1)
+        axe.GetYAxisShaftProperty().SetColor(0,0,1)
+        axe.GetZAxisShaftProperty().SetColor(0,0,1)
+
+        axe.GetXAxisTipProperty().SetColor(0,0,1)
+        axe.GetYAxisTipProperty().SetColor(0,0,1)
+        axe.GetZAxisTipProperty().SetColor(0,0,1)
+
+        axe2 = vtk.vtkAxesActor()
+        axe2.AxisLabelsOff()
+        axe2.SetTotalLength(0.18*have_momento[0], 0.18*have_momento[1], 0.18*have_momento[2])
+        axe2.SetShaftTypeToLine()
+        axe2.SetNormalizedTipLength(0.2*have_momento[0], 0.2*have_momento[1], 0.2*have_momento[2])
+        axe2.SetUserTransform(transform)
+
+        axe2.GetXAxisShaftProperty().SetColor(0,0,1)
+        axe2.GetYAxisShaftProperty().SetColor(0,0,1)
+        axe2.GetZAxisShaftProperty().SetColor(0,0,1)
+
+        axe2.GetXAxisTipProperty().SetColor(0,0,1)
+        axe2.GetYAxisTipProperty().SetColor(0,0,1)
+        axe2.GetZAxisTipProperty().SetColor(0,0,1)
+
+        self.axes[key_id].append(axe)
+        self.axes[key_id].append(axe2)
+
+        self._renderer.AddActor(axe)
+        self._renderer.AddActor(axe2)
+
+    def plotForceAxes(self, node, key_id):
+        force = self.getReal(node.get_prescribed_loads())
+        have_force = [0,0,0]
+        negative_force = [0,0,0]
+        for i in range(0,3):
+            if force[i] is not None and force[i] != 0:
+                have_force[i] = 1
+            if force[i] is not None and force[i] < 0:
+                negative_force[i] = 1
+
+        if have_force.count(0) == 3:
+            return
+
+        transform = self.transformAxe(node, negative_force)
+        have_force = self.getPosAxe(negative_force, have_force)
+        
+        axe = vtk.vtkAxesActor()
+        axe.AxisLabelsOff()
+        axe.SetTotalLength(0.15*have_force[0], 0.15*have_force[1], 0.15*have_force[2])
+        axe.SetShaftTypeToLine()
+        axe.SetShaftTypeToCylinder()
+        axe.SetCylinderRadius(self.getSize()*2)
+        axe.SetNormalizedTipLength(0.2*have_force[0], 0.2*have_force[1], 0.2*have_force[2])
+        axe.SetUserTransform(transform)
+
+        axe.GetXAxisShaftProperty().SetColor(1,0,0)
+        axe.GetYAxisShaftProperty().SetColor(1,0,0)
+        axe.GetZAxisShaftProperty().SetColor(1,0,0)
+
+        axe.GetXAxisTipProperty().SetColor(1,0,0)
+        axe.GetYAxisTipProperty().SetColor(1,0,0)
+        axe.GetZAxisTipProperty().SetColor(1,0,0)
+
+        self.axes[key_id].append(axe)
+        self._renderer.AddActor(axe)
