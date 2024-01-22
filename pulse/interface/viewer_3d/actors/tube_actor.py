4c34da6e166c8ee311b4c720c61dca11e23e64a0
André Fernandes <fpf.andre@gmail.com>
2024-01-22 17:17:34 -0300

    refactor tube_actor to have simpler and faster sections without needing to use gpu

--- pulse/interface/viewer_3d/actors/tube_actor.py
+++ pulse/interface/viewer_3d/actors/tube_actor.py
@@ -14,43 +14,91 @@ class TubeActor(vtk.vtkActor):
 
     def build(self):
         visible_elements = {i:e for i, e in self.elements.items() if (i not in self.hidden_elements)}
-        self._key_index  = {j:i for i,j in enumerate(visible_elements)}
+        # self._key_index  = {j:i for i,j in enumerate(visible_elements)}
+        self._key_indexes = dict()
 
-        data = vtk.vtkPolyData()
-        mapper = vtk.vtkGlyph3DMapper()
+        mapper = vtk.vtkPolyDataMapper()
+        append_polydata = vtk.vtkAppendPolyData()
 
-        points = vtk.vtkPoints()
-        sources = vtk.vtkIntArray()
-        sources.SetName('sources')
-        rotations = vtk.vtkDoubleArray()
-        rotations.SetNumberOfComponents(3)
-        rotations.SetName('rotations')
+        total_points_appended = 0 
+        for i, element in visible_elements.items():            
+            x,y,z = element.first_node.coordinates
+            section_rotation_xyz = element.section_rotation_xyz_undeformed
 
-        for element in visible_elements.values():
-            points.InsertNextPoint(*element.first_node.coordinates)
-            rotations.InsertNextTuple(element.section_rotation_xyz_undeformed)
-            sources.InsertNextTuple1(0)
+            source = self.create_element_data(element)
+            self._key_indexes[i] = range(total_points_appended, total_points_appended + source.GetNumberOfPoints())
+            total_points_appended += source.GetNumberOfPoints()
 
-        source = self.create_element_data(element)
-        mapper.SetSourceData(0, source)
+            # for _ in range(source.GetNumberOfPoints()):
+            #     self._colors.InsertNextTuple((255,255,255))
 
-        data.SetPoints(points)
-        data.GetPointData().AddArray(sources)
-        data.GetPointData().AddArray(rotations)
-        # data.GetPointData().SetScalars(self._colors)
+            transform = vtk.vtkTransform()
+            transform.Translate((x,y,z))
+            transform.RotateX(section_rotation_xyz[0])
+            transform.RotateZ(section_rotation_xyz[2])
+            transform.RotateY(section_rotation_xyz[1])
+            transform.RotateZ(90)
 
-        mapper.SetInputData(data)
-        mapper.SourceIndexingOn()
-        mapper.SetSourceIndexArray('sources')
-        mapper.SetOrientationArray('rotations')
-        mapper.SetScaleFactor(1)
-        mapper.SetOrientationModeToRotation()
-        mapper.Update()
+            transform.Update()
 
+            transform_filter = vtk.vtkTransformFilter()
+            transform_filter.SetInputData(source)
+            transform_filter.SetTransform(transform)
+            transform_filter.Update()
+
+            append_polydata.AddInputData(transform_filter.GetOutput())
+        
+        append_polydata.Update()
+        mapper.SetInputData(append_polydata.GetOutput())
         self.SetMapper(mapper)
 
     def create_element_data(self, element):
-        sphere = vtk.vtkSphereSource()
-        sphere.SetRadius(0.1)
-        sphere.Update()
-        return sphere.GetOutput()
+        return self.pipe_data(element.length, 0.08, 0.001)
+        # sphere = vtk.vtkSphereSource()
+        # sphere.SetRadius(0.1)
+        # sphere.Update()
+        # return sphere.GetOutput()
+
+    def pipe_data(self, length, outside_diameter, thickness):
+        cilinder = vtk.vtkCylinderSource()
+        cilinder.SetResolution(20)
+        cilinder.SetRadius(outside_diameter / 2)
+        cilinder.SetHeight(length)
+        if thickness == 0:
+            cilinder.CappingOn()
+        else:
+            cilinder.CappingOff()
+        cilinder.Update()
+        return cilinder.GetOutput()
+
+    def circular_beam_data(self, length, outside_diameter, thickness):
+        return self.pipe_data(length, )
+
+    def square_beam_data(self, length, base, height, thickness):
+        pass
+
+    def c_beam_data(self, length, h, w1, w2, t1, t2, tw):
+        pass
+
+    def i_beam_data(self, length, h, w1, w2, t1, t2, tw):
+        pass
+
+    def t_section(self, length, h, w1, t1, tw):
+        '''
+        bottom:             top:
+        0          1        8          9
+        ┌──────────┐        ┌──────────┐
+        │          │        │          │
+       2└──┐    ┌──┘3     10└──┐    ┌──┘11
+          4│    │5           12│    │13
+           │    │              │    │
+           │    │              │    │
+           │    │              │    │
+          6└────┘7           14└────┘15
+        '''
+
+        # data = vtk.vtkPolyData()
+        # points = vtk.vtkPoints()
+        # points.SetNumberOfPoints(16)
+
+        # points.SetPoint(0, -w1/2, )
