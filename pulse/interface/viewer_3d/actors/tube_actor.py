a6ccfe22e8dabef4447494683057870d340954c7
André Fernandes <fpf.andre@gmail.com>
2024-01-23 12:49:02 -0300

    move cross section sources to a dedicated file

--- pulse/interface/viewer_3d/actors/tube_actor.py
+++ pulse/interface/viewer_3d/actors/tube_actor.py
@@ -1,5 +1,7 @@
 import vtk
 from vtkat.utils import set_polydata_property, set_polydata_colors
+from pulse.interface.viewer_3d.sources import cross_section_sources
+
 
 class TubeActor(vtk.vtkActor):
     def __init__(self, project, **kwargs) -> None:
@@ -68,28 +70,28 @@ class TubeActor(vtk.vtkActor):
         if cross_section.section_label == "Pipe section":
             d_out = cross_section.outer_diameter
             t = cross_section.thickness
-            return self.pipe_data(element.length, d_out, t)
+            return cross_section_sources.pipe_data(element.length, d_out, t)
 
         elif cross_section.section_label == "Rectangular section":
             b, h, *_ = cross_section.section_parameters
-            return self.rectangular_beam_data(element.length, b, h)
+            return cross_section_sources.rectangular_beam_data(element.length, b, h)
 
         elif cross_section.section_label == "Circular section":
             d_out, t, *_ = cross_section.section_parameters
-            return self.circular_beam_data(element.length, d_out, t)
+            return cross_section_sources.circular_beam_data(element.length, d_out, t)
 
         elif cross_section.section_label == "C-section":
             h, w1, t1, w2, t2, tw, *_ = cross_section.section_parameters
-            return self.c_beam_data(element.length, h, w1, w2, t1, t2, tw)
+            return cross_section_sources.c_beam_data(element.length, h, w1, w2, t1, t2, tw)
 
         elif cross_section.section_label == "I-section":
 
             h, w1, t1, w2, t2, tw, *_ = cross_section.section_parameters
-            return self.i_beam_data(element.length, h, w1, w2, t1, t2, tw)
+            return cross_section_sources.i_beam_data(element.length, h, w1, w2, t1, t2, tw)
 
         elif cross_section.section_label == "T-section":
             h, w1, t1, tw, *_ = cross_section.section_parameters
-            return self.t_beam_data(element.length, h, w1, t1, tw)
+            return cross_section_sources.t_beam_data(element.length, h, w1, t1, tw)
 
         return None
 
@@ -119,164 +121,3 @@ class TubeActor(vtk.vtkActor):
 
     def set_color_table(self, color_table):
         pass
-
-    # 
-    def pipe_data(self, length, outside_diameter, thickness):
-        if thickness == 0:
-            return self.closed_pipe_data(length, outside_diameter)
-        cilinder = vtk.vtkCylinderSource()
-        cilinder.SetResolution(20)
-        cilinder.SetRadius(outside_diameter / 2)
-        cilinder.SetHeight(length)
-        cilinder.CappingOff()
-        cilinder.Update()
-        return cilinder.GetOutput()
-
-    def closed_pipe_data(self, length, outside_diameter):
-        cilinder = vtk.vtkCylinderSource()
-        cilinder.SetResolution(20)
-        cilinder.SetRadius(outside_diameter / 2)
-        cilinder.SetHeight(length)
-        cilinder.CappingOn()
-        cilinder.Update()
-        return cilinder.GetOutput()
-
-    def circular_beam_data(self, length, outside_diameter, thickness):
-        return self.pipe_data(length, outside_diameter, thickness)
-
-    def rectangular_beam_data(self, length, b, h, t):
-        if t == 0:
-            return self.closed_rectangular_beam_data(length, b, h)
-
-        rectangular_top = vtk.vtkCubeSource()
-        rectangular_left = vtk.vtkCubeSource()
-        rectangular_right = vtk.vtkCubeSource()
-        rectangular_bottom = vtk.vtkCubeSource()
-
-        rectangular_top.SetYLength(length)
-        rectangular_top.SetZLength(t)
-        rectangular_top.SetXLength(b)
-        rectangular_top.SetCenter(0, 0, -h/2 + t/2)
-
-        rectangular_left.SetYLength(length)
-        rectangular_left.SetZLength(h)
-        rectangular_left.SetXLength(t)
-        rectangular_left.SetCenter(-b/2 + t/2, 0, 0)
-
-        rectangular_right.SetYLength(length)
-        rectangular_right.SetZLength(h)
-        rectangular_right.SetXLength(t)
-        rectangular_right.SetCenter(b/2 - t/2, 0, 0)
-
-        rectangular_bottom.SetYLength(length)
-        rectangular_bottom.SetZLength(t)
-        rectangular_bottom.SetXLength(b)
-        rectangular_bottom.SetCenter(0, 0, h/2 - t/2)
-
-        rectangular_top.Update()
-        rectangular_left.Update()
-        rectangular_right.Update()
-        rectangular_bottom.Update()
-
-        append_polydata = vtk.vtkAppendPolyData()
-        append_polydata.AddInputData(rectangular_top.GetOutput())
-        append_polydata.AddInputData(rectangular_left.GetOutput())
-        append_polydata.AddInputData(rectangular_right.GetOutput())
-        append_polydata.AddInputData(rectangular_bottom.GetOutput())
-        append_polydata.Update()
-
-        return append_polydata.GetOutput()
-
-    def closed_rectangular_beam_data(self, length, b, h):
-        rectangular = vtk.vtkCubeSource()
-        rectangular.SetYLength(length)
-        rectangular.SetXLength(b)
-        rectangular.SetZLength(h)
-        rectangular.Update()
-        return rectangular.GetOutput()
-
-    def c_beam_data(self, length, h, w1, w2, t1, t2, tw):
-        rectangular_top = vtk.vtkCubeSource()
-        rectangular_left = vtk.vtkCubeSource()
-        rectangular_bottom = vtk.vtkCubeSource()
-
-        rectangular_top.SetYLength(length)
-        rectangular_top.SetZLength(t1)
-        rectangular_top.SetXLength(w1)
-        rectangular_top.SetCenter(w1/2 - max(w1, w2)/2, 0, -h/2 + t1/2)
-
-        rectangular_left.SetYLength(length)
-        rectangular_left.SetZLength(h)
-        rectangular_left.SetXLength(tw)
-        rectangular_left.SetCenter(-max(w1, w2)/2 + tw/2, 0, 0)
-
-        rectangular_bottom.SetYLength(length)
-        rectangular_bottom.SetZLength(t2)
-        rectangular_bottom.SetXLength(w2)
-        rectangular_bottom.SetCenter(w2/2 - max(w1, w2)/2, 0, h/2 - t2/2)
-
-        rectangular_top.Update()
-        rectangular_left.Update()
-        rectangular_bottom.Update()
-
-        append_polydata = vtk.vtkAppendPolyData()
-        append_polydata.AddInputData(rectangular_top.GetOutput())
-        append_polydata.AddInputData(rectangular_left.GetOutput())
-        append_polydata.AddInputData(rectangular_bottom.GetOutput())
-        append_polydata.Update()
-
-        return append_polydata.GetOutput()
-
-    def i_beam_data(self, length, h, w1, w2, t1, t2, tw):
-        rectangular_top = vtk.vtkCubeSource()
-        rectangular_center = vtk.vtkCubeSource()
-        rectangular_bottom = vtk.vtkCubeSource()
-
-        rectangular_top.SetYLength(length)
-        rectangular_top.SetZLength(t1)
-        rectangular_top.SetXLength(w1)
-        rectangular_top.SetCenter(0, 0, -h/2 + t1/2)
-
-        rectangular_center.SetYLength(length)
-        rectangular_center.SetZLength(h)
-        rectangular_center.SetXLength(tw)
-
-        rectangular_bottom.SetYLength(length)
-        rectangular_bottom.SetZLength(t2)
-        rectangular_bottom.SetXLength(w2)
-        rectangular_bottom.SetCenter(0, 0, h/2 - t2/2)
-
-        rectangular_top.Update()
-        rectangular_center.Update()
-        rectangular_bottom.Update()
-
-        append_polydata = vtk.vtkAppendPolyData()
-        append_polydata.AddInputData(rectangular_top.GetOutput())
-        append_polydata.AddInputData(rectangular_center.GetOutput())
-        append_polydata.AddInputData(rectangular_bottom.GetOutput())
-        append_polydata.Update()
-
-        return append_polydata.GetOutput()
-
-    def t_beam_data(self, length, h, w1, t1, tw):
-        rectangular_top = vtk.vtkCubeSource()
-        rectangular_center = vtk.vtkCubeSource()
-
-        rectangular_top.SetYLength(length)
-        rectangular_top.SetZLength(t1)
-        rectangular_top.SetXLength(w1)
-        rectangular_top.SetCenter(0, 0, -h/2 + t1/2)
-
-        rectangular_center.SetYLength(length)
-        rectangular_center.SetZLength(h)
-        rectangular_center.SetXLength(tw)
-
-        rectangular_top.Update()
-        rectangular_center.Update()
-
-        append_polydata = vtk.vtkAppendPolyData()
-        append_polydata.AddInputData(rectangular_top.GetOutput())
-        append_polydata.AddInputData(rectangular_center.GetOutput())
-        append_polydata.Update()
-
-        return append_polydata.GetOutput()
