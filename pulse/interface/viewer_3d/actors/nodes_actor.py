6d7fa4d280ae6580109cb08245a74584a611c75a
André Fernandes <fpf.andre@gmail.com>
2024-01-24 14:19:28 -0300

    refactor nodes actor

--- pulse/interface/viewer_3d/actors/nodes_actor.py
+++ pulse/interface/viewer_3d/actors/nodes_actor.py
@@ -1,22 +1,40 @@
 import vtk
-from vtkat.actors import RoundPointsActor
+from vtkat.poly_data import VerticesData
 
 
-class NodesActor(RoundPointsActor):
+class NodesActor(vtk.vtkActor):
     def __init__(self, project, **kwargs) -> None:
+        super().__init__()
         self.project = project
         self.nodes = project.get_nodes()
         self.hidden_nodes = kwargs.get('hidden_nodes', set())
-
-        points = self.get_points()
-        super().__init__(points)
-
-        self.appear_in_front(True)
-        self.GetProperty().SetPointSize(8)
-        self.GetProperty().SetColor([i/255 for i in (255, 180, 50)])
+        self.build()
     
-    def get_points(self):
+    def build(self):
         visible_nodes = {i:e for i,e in self.nodes.items() if (i not in self.hidden_nodes)}
         self._key_index = {j:i for i,j in enumerate(visible_nodes)}
         coords = [n.coordinates for n in visible_nodes.values()]
-        return coords
+        data = VerticesData(coords)
+        mapper = vtk.vtkPolyDataMapper()
+        mapper.SetInputData(data)
+
+        self.SetMapper(mapper)
+        self.GetProperty().SetPointSize(8)
+        self.GetProperty().RenderPointsAsSpheresOn()
+        self.GetProperty().RenderPointsAsSpheresOn()
+        self.GetProperty().LightingOff()
+        self.GetProperty().SetColor([i/255 for i in (255, 180, 50)])
+        self.appear_in_front(True)
+
+    def appear_in_front(self, cond: bool):
+        # this offset is the Z position of the camera buffer.
+        # if it is -66000 the object stays in front of everything.
+        offset = -66000 if cond else 0
+        mapper = self.GetMapper()
+        mapper.SetResolveCoincidentTopologyToPolygonOffset()
+        mapper.SetRelativeCoincidentTopologyLineOffsetParameters(0, offset)
+        mapper.SetRelativeCoincidentTopologyPolygonOffsetParameters(0, offset)
+        mapper.SetRelativeCoincidentTopologyPointOffsetParameter(offset)
+
+    def set_color(self, color, nodes=None):
+        pass
