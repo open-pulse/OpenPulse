4016aa886c66371aeda1afd4ccce8d15a48d06de
André Fernandes <fpf.andre@gmail.com>
2024-06-14 16:00:25 -0300

    simplify logo creation

--- pulse/interface/viewer_3d/render_widgets/geometry_render_widget.py
+++ pulse/interface/viewer_3d/render_widgets/geometry_render_widget.py
@@ -13,22 +13,6 @@ class GeometryRenderWidget(EditorRenderWidget):
 
     def create_logos(self, theme="light"):
         self.renderer.RemoveViewProp(self.open_pulse_logo)
-        open_pulse_path = str(ICON_DIR/ 'logos/OpenPulse_logo_gray.png')
-
-        self.open_pulse_logo = self._load_vtk_logo(open_pulse_path)
+        self.open_pulse_logo = self.create_logo(ICON_DIR/ 'logos/OpenPulse_logo_gray.png')
         self.open_pulse_logo.SetPosition(0.845, 0.89)
         self.open_pulse_logo.SetPosition2(0.15, 0.15)
-        self.renderer.AddViewProp(self.open_pulse_logo)
-        self.open_pulse_logo.SetRenderer(self.renderer)
-
-    def _load_vtk_logo(self, path):
-        image_reader = vtk.vtkPNGReader()
-        image_reader.SetFileName(path)
-        image_reader.Update()
-
-        logo = vtk.vtkLogoRepresentation()
-        logo.SetImage(image_reader.GetOutput())
-        logo.ProportionalResizeOn()
-        logo.GetImageProperty().SetOpacity(0.9)
-        logo.GetImageProperty().SetDisplayLocationToBackground()
-        return logo
