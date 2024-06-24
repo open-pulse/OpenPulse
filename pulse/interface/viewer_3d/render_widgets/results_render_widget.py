c846d2c660a5d5584481b917ddec62274b66faf1
André Fernandes <fpf.andre@gmail.com>
2024-06-24 14:03:02 -0300

    implement the methods to show all analysis

--- pulse/interface/viewer_3d/render_widgets/results_render_widget.py
+++ pulse/interface/viewer_3d/render_widgets/results_render_widget.py
@@ -14,6 +14,7 @@ from pulse.interface.viewer_3d.text_helppers import TreeInfo, format_long_sequen
 from pulse.postprocessing.plot_structural_data import (
     get_structural_response,
     get_stresses_to_plot,
+    get_min_max_stresses_values,
     get_min_max_resultant_displacements,
 )
 from pulse.postprocessing.plot_acoustic_data import (
@@ -83,19 +84,23 @@ class ResultsRenderWidget(AnimatedRenderWidget):
 
         # update the data according to the current analysis
         if self.analysis_mode == AnalysisMode.DISPLACEMENT:
-            self._compute_displacement_field(self.current_frequency_index, self.current_phase_step)
+            color_table = self._compute_displacement_field(self.current_frequency_index, self.current_phase_step)
 
         elif self.analysis_mode == AnalysisMode.STRESS:
-            self._compute_stress_field(self.current_frequency_index, self.current_phase_step)
+            color_table = self._compute_stress_field(self.current_frequency_index, self.current_phase_step)
 
         elif self.analysis_mode == AnalysisMode.PRESURE:
-            self._compute_pressure_field(self.current_frequency_index, self.current_phase_step)
+            color_table = self._compute_pressure_field(self.current_frequency_index, self.current_phase_step)
+
+        else:
+            # Empty color table
+            color_table = ColorTable(project, [], [0, 0], self.colormap)
+
 
         self.tubes_actor = TubeActorGPU(project)
 
         self.renderer.AddActor(self.tubes_actor)
 
-        color_table = ColorTable(project, self.u_def, [self._result_min, self._result_max], self.colormap)
         self.colorbar_actor.SetLookupTable(color_table)
         self.tubes_actor.set_color_table(color_table)
 
@@ -113,37 +118,36 @@ class ResultsRenderWidget(AnimatedRenderWidget):
         self.current_phase_step = 0
         self.analysis_mode = AnalysisMode.DISPLACEMENT
 
+        self._reset_min_max_values()
         tmp = get_min_max_resultant_displacements(solution, frequency_index)
-        _, self._result_min, self._result_max, self.r_xyz_abs = tmp
+        _, self.result_disp_min, self.result_disp_max, self.r_xyz_abs = tmp
 
         self.update_plot()
 
     def show_stress_field(self, frequency_index):
-        return
         solution = app().main_window.project.get_structural_solution()
 
         self.current_frequency_index = frequency_index
         self.current_phase_step = 0
         self.analysis_mode = AnalysisMode.STRESS
 
+        self._reset_min_max_values()
+        self.stress_min, self.stress_max = get_min_max_stresses_values()
         tmp = get_min_max_resultant_displacements(solution, frequency_index)
-        _, self._result_min, self._result_max, self.r_xyz_abs = tmp
+        _, self.result_disp_min, self.result_disp_max, self.r_xyz_abs = tmp
 
         self.update_plot()
 
     def show_pressure_field(self, frequency_index):
-        return
-        solution = app().main_window.project.get_structural_solution()
+        solution = app().main_window.project.get_acoustic_solution()
 
         self.current_frequency_index = frequency_index
         self.current_phase_step = 0
         self.analysis_mode = AnalysisMode.PRESURE
 
+        self._reset_min_max_values()
         tmp = get_max_min_values_of_pressures(solution, frequency_index)
         self.pressure_min, self.pressure_max = tmp
-
-        tmp = get_max_min_values_of_pressures(solution, frequency_index)
-        _, self._result_min, self._result_max, self.r_xyz_abs = tmp
 
         self.update_plot()
 
@@ -172,7 +176,7 @@ class ResultsRenderWidget(AnimatedRenderWidget):
         # effects. It interprets the structural analysis and puts the meaningfull
         # data inside the correspondent nodes.
         # The return values are just extra information.
-        _, _, self.u_def, self._magnification_factor = get_structural_response(
+        _, _, u_def, self._magnification_factor = get_structural_response(
             preprocessor,
             solution,
             frequency_index, 
@@ -180,15 +184,44 @@ class ResultsRenderWidget(AnimatedRenderWidget):
             r_max = self.r_xyz_abs
         )
 
+        color_table = ColorTable(
+            project,
+            u_def,
+            (self.result_disp_min, self.result_disp_max),
+            self.colormap,
+            stress_field_plot=True,   
+        )
+
+        return color_table
+
     def _compute_stress_field(self, frequency_index, phase_step):
-        return
         project = app().project
         preprocessor = project.preprocessor
         solution = project.get_structural_solution()
-        get_stresses_to_plot(phase_step)
+        
+        *_, self._magnification_factor = get_structural_response(
+            preprocessor,
+            solution,
+            frequency_index, 
+            phase_step = phase_step,
+            r_max = self.r_xyz_abs
+        )
+
+        stresses_data, self.min_max_stresses_values_current = get_stresses_to_plot(
+            phase_step=phase_step
+        )
+
+        color_table = ColorTable(
+            project,
+            stresses_data,
+            (self.stress_min, self.stress_max),
+            self.colormap,
+            stress_field_plot=True,   
+        )
+        return color_table
+
 
     def _compute_pressure_field(self, frequency_index, phase_step):
-        return
         project = app().project
         preprocessor = project.preprocessor
         solution = project.get_acoustic_solution()
@@ -197,4 +230,28 @@ class ResultsRenderWidget(AnimatedRenderWidget):
             preprocessor,
             solution,
             frequency_index, 
-            phase_step=phase_step  )
+            phase_step=phase_step
+        )
+
+        color_table = ColorTable(
+            project,
+            pressure_field_data,
+            (self.pressure_min, self.pressure_max),
+            self.colormap,
+            pressure_field_plot=True,   
+        )
+        
+        return color_table
+
+    def _reset_min_max_values(self):
+        self.count_cycles = 0
+        self.r_xyz_abs = None
+        self.result_disp_min = None
+        self.result_disp_max = None
+        self.stress_min = None
+        self.stress_max = None
+        self.pressure_min = None
+        self.pressure_max = None
+        self.min_max_stresses_values_current = None
+        self.min_max_pressures_values_current = None
+        self.plot_state = [False, False, False]
