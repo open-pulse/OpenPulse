fa7342a095b09581865b72054826a7c1542cbde0
André Fernandes <fpf.andre@gmail.com>
2024-09-10 11:38:03 -0300

    add c beam options

--- pulse/interface/user_input/model/geometry/options/__init__.py
+++ pulse/interface/user_input/model/geometry/options/__init__.py
@@ -2,3 +2,4 @@ from .structure_options import StructureOptions
 from .pipe_options import PipeOptions
 from .t_beam_options import TBeamOptions
 from .i_beam_options import IBeamOptions
+from .c_beam_options import CBeamOptions
