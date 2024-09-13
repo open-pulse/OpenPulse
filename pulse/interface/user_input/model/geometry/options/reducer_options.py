634184cbf1ac475fca83e7dc91d2eed55f9f63d9
André Fernandes <fpf.andre@gmail.com>
2024-09-13 14:36:04 -0300

    improve control over geometry editor buttons

--- pulse/interface/user_input/model/geometry/options/reducer_options.py
+++ pulse/interface/user_input/model/geometry/options/reducer_options.py
@@ -66,7 +66,15 @@ class ReducerOptions(StructureOptions):
             set_qproperty(self.geometry_designer_widget.configure_button, warning=True, status="danger")
             enable = False
 
-        self.geometry_designer_widget.create_structure_frame.setEnabled(enable)
+        enable_attach = len(self.pipeline.selected_points) >= 2
+        enable_add = len(self.pipeline.staged_structures) + len(self.pipeline.staged_points) >= 1
+        enable_delete = len(self.pipeline.selected_structures) + len(self.pipeline.selected_points) >= 1
+
+        self.geometry_designer_widget.frame_bending_options.setEnabled(enable)
+        self.geometry_designer_widget.frame_bounding_box_sizes.setEnabled(enable)
+        self.geometry_designer_widget.attach_button.setEnabled(enable_attach)
+        self.geometry_designer_widget.add_button.setEnabled(enable_add)
+        self.geometry_designer_widget.delete_button.setEnabled(enable_delete)
 
     def _get_kwargs(self) -> dict:
         if self.cross_section_info is None:
