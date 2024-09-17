ccaa46fd0039185d0919fee3d4064d9a5f2e60c9
André Fernandes <fpf.andre@gmail.com>
2024-09-17 15:48:48 -0300

    fix point configuration button

--- pulse/interface/user_input/model/geometry/options/point_options.py
+++ pulse/interface/user_input/model/geometry/options/point_options.py
@@ -5,6 +5,7 @@ if TYPE_CHECKING:
 from opps.model import Point
 
 from .structure_options import StructureOptions
+from molde.stylesheets import set_qproperty
 
 
 class PointOptions(StructureOptions):
@@ -33,6 +34,8 @@ class PointOptions(StructureOptions):
         pass
 
     def update_permissions(self):
+        set_qproperty(self.geometry_designer_widget.configure_button, warning=False, status="default")
+        self.geometry_designer_widget.configure_button.setEnabled(False)
         self.geometry_designer_widget.frame_bending_options.setEnabled(True)
         self.geometry_designer_widget.frame_bounding_box_sizes.setEnabled(True)
         self.geometry_designer_widget.attach_button.setEnabled(True)
