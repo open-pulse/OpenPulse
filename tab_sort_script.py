ee861dd78a8336ab661b04b0cbba4544335c883c
Rodrigo Schwartz <rodrigo32003@gmail.com>
2024-10-31 17:22:47 -0300

    Adds the first part of tab_sort_script

--- tab_sort_script.py
+++ tab_sort_script.py
@@ -2,6 +2,10 @@ from pathlib import Path
 
 import re
 
+def tab_sort_script():
+    ui_files = find_ui_files()
+    tab_sorting(ui_files)
+
 def find_ui_files():
     ui_files_path = list()
     for path in Path("pulse/interface/data/ui_files").rglob("*.ui"):
@@ -11,20 +15,55 @@ def find_ui_files():
 
 def tab_sorting(ui_files: list[Path]):
     for file in ui_files:
-        answer = extract_tabs(file)   
+        old_tab_string, new_tab_string, arch_content = extract_tabs(file)
+        write_tab_string_in_ui_file(file, old_tab_string, new_tab_string, arch_content)   
 
-def tab_sort_script():
-    ui_files = find_ui_files()
-    tab_sorting(ui_files)
 
 def extract_tabs(ui_file: Path):
     arch_content = ui_file.read_text()
+
     tab_regex = re.compile(r"<tabstops>(.|\n)*</tabstops>")
     contains_tab = tab_regex.search(arch_content)
 
     if contains_tab is not None:
-        tabstop_regex = re.compile(r"<tabstop>.*</tabstop")
-        widgets_names = tabstop_regex.findall(str(contains_tab))
-        print(widgets_names)
+        old_tab_string = contains_tab.group(0)
+
+        tabstop_regex = re.compile(r"<tabstop>.*</tabstop>")
+        widgets = tabstop_regex.findall(str(arch_content))
+
+        widgets_name = list()
+        for widget in widgets:
+            widgets_name.append(widget[9:len(widget)-10])
+        
+        widgets_name.sort(key=arch_content.find)
+        new_tab_string = generate_tab_string(widgets_name)
+
+        return old_tab_string, new_tab_string, arch_content
+    
+    widgets_name = find_widgets(arch_content)
+       
+def generate_tab_string(widgets_names: list[str]):
+    text = "<tabstops>\n"
+    for widget_name in widgets_names:
+        text += f"  <tabstop>{widget_name}</tabstop>\n"
+    text += "</tabstops>"
+
+    return text
+
+def find_widgets(archive_content: str):
+    old_string_regex = re.compile(r"<resources/>(.|\n)*") 
+    old_string = old_string_regex.search(archive_content).group(0)
+
+    widgets_regex = re.compile(r"<widget.*")
+    widgets = widgets_regex.findall(archive_content)
+
+    widgets_name = list()
+
+def write_tab_string_in_ui_file(ui_file: Path, old_tab_string: str, new_tab_string: str, arch_content: str):
+    arch_content = arch_content.replace(old_tab_string, new_tab_string)
+
+    archive = open(ui_file, "w")
+    archive.write(arch_content)
+    archive.close()
 
 tab_sort_script()
