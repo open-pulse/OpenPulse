a3e5153461fb2900d399b9844738d68725e868ed
André Fernandes <fpf.andre@gmail.com>
2024-11-05 19:51:25 -0300

    sketch arc bend structure

---
+++ pulse/editor/structures/arc_bend.py
@@ -0,0 +1,22 @@
+from molde.colors import PURPLE_7
+from .curve import Curve
+
+
+class ArcBend(Curve):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.diameter = kwargs.get("diameter", 0.1)
+        self.thickness = kwargs.get("thickness", 0.01)
+        self.color = PURPLE_7
+
+    def as_dict(self) -> dict:
+        return super().as_dict() | {
+            "diameter": self.diameter,
+            "thickness": self.thickness,
+        }
+
+    def as_vtk(self):
+        from pulse.interface.viewer_3d.actors import BendActor
+        # ArcBends also have a center point, so the actor is the same
+        return BendActor(self)
