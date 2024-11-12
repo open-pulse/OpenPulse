c5eadca3c5b765f4e12cacfe4ed425d599111b03
André Fernandes <fpf.andre@gmail.com>
2024-11-12 12:58:37 -0300

    implement function to load ArcBend from data

--- pulse/editor/structures/arc_bend.py
+++ pulse/editor/structures/arc_bend.py
@@ -1,5 +1,6 @@
 from molde.colors import PURPLE_7
 from .arc import Arc
+from .point import Point
 
 
 class ArcBend(Arc):
@@ -20,3 +21,24 @@ class ArcBend(Arc):
         from pulse.interface.viewer_3d.actors import ArcBendActor
 
         return ArcBendActor(self)
+
+    @classmethod
+    def load_from_data(cls, data: dict) -> "ArcBend":
+        start = Point(*data["start_coords"])
+        end = Point(*data["end_coords"])
+        mid = Point(*data["mid_coords"])
+        section_parameters = data["section_parameters"]
+        structure = ArcBend(
+            start,
+            end,
+            mid,
+            diameter=section_parameters[0],
+            thickness=section_parameters[1],
+        )
+        section_info = {
+            "section_type_label": data["section_type_label"],
+            "section_parameters": section_parameters,
+        }
+        structure.extra_info["cross_section_info"] = section_info
+        structure.extra_info["structural_element_type"] = "pipe_1"
+        return structure
