d72692e0481deb55dfc656a4cd03ca5f1924b67f
André Fernandes <fpf.andre@gmail.com>
2024-12-04 18:20:45 -0300

    add a border after fitting new points

--- pulse/interface/user_input/model/geometry/geometry_designer_widget.py
+++ pulse/interface/user_input/model/geometry/geometry_designer_widget.py
@@ -381,6 +381,7 @@ class GeometryDesignerWidget(QWidget):
         x0, y0 = 0, 0
         x1, y1 = w, h
         need_to_fit = False
+        border = int(max(w, h) * 0.1)
 
         for point in chain(self.pipeline.staged_points, self.pipeline.selected_points):
             coordinate.SetValue(*point)
@@ -395,6 +396,16 @@ class GeometryDesignerWidget(QWidget):
             
         if not need_to_fit:
             return
+        
+        if x0 < 0:
+            x0 -= border
+        elif x1 > w:
+            x1 += border
+        
+        if y0 < 0:
+            y0 -= border
+        elif y1 > h:
+            y1 += border
         
         dx = (x1 + x0 - w) / 2
         dy = (y1 + y0 - h) / 2
