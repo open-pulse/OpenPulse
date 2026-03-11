ea4831267033fcce3d70240d8c7674aba315878f
R0drigoSchwartz <rodrigo32003@gmail.com>
2026-03-10 21:08:14 -0300

    Adds ElementsInput class

---
+++ pulse/interface/user_input/model/setup/elements_input.py
@@ -0,0 +1,23 @@
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QLineEdit
+
+from pulse import app
+from pulse.interface.user_input.model.setup.user_input import UserInput
+
+
+class ElementsInput(UserInput):
+    def __init__(self):
+        super().__init__()
+
+        self.properties = app().project.model.properties
+        self.preprocessor = app().project.model.preprocessor
+
+        self.before_run = app().project.get_pre_solution_model_checks()
+    
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Enter or event.key() == Qt.Key_Return:
+            self.attribute_callback()
+        elif event.key() == Qt.Key_Delete:
+            self.remove_callback()
+        elif event.key() == Qt.Key_Escape:
+            self.close()
