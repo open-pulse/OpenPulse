5cb74629e7c5e9b91e53c280f96200cc3073daec
R0drigoSchwartz <rodrigo32003@gmail.com>
2026-03-14 09:14:35 -0300

    Updates UserInput class

--- pulse/interface/user_input/model/setup/user_input.py
+++ pulse/interface/user_input/model/setup/user_input.py
@@ -106,6 +106,71 @@ class UserInput(QDialog):
             PrintMessageInput([error_title, title, message])
             line_edit.setFocus()
             return None, None
+    
+    def get_spectral_data_from_array(self, data: np.ndarray | None, return_frequencies: bool=False):
+        """
+        This function returns two vectors containing the spectral data of interest.
+        The first one is the frequencies vector and the second is the vector of 
+        complex values.
+        
+        Parameters
+        ----------
+        data : np.ndarray
+            The array that gathers spectral data.
+
+        return_frequencies: bool, optional
+            It controls whether the frequencies vector will be returned.
+        """
+        if data is None:
+            return None
+    
+        complex_values = data[:, 1] + 1j * data[:, 2]
+
+        if return_frequencies:
+            frequencies = data[:, 0]
+            return frequencies, complex_values
+
+        return complex_values
+
+
+    def update_analysis_setup_in_file(self, frequencies: np.ndarray):
+
+        analysis_setup = app().project.file.read_analysis_setup_from_file()
+
+        analysis_setup.update(
+            {
+            "frequency_spacing" : "tabular",
+            "f_min" : float(frequencies[0]),
+            "f_max" : float(frequencies[-1]),
+            "f_step" : float(frequencies[1] - frequencies[0]),
+            "frequencies" : None,
+            }
+            )
+
+        app().project.model.set_analysis_setup(analysis_setup)
+        app().project.file.write_analysis_setup_in_file(analysis_setup)
+
+
+    def get_table_name(self, _label: str, node_id: int | None = None, element_id: int | None = None, line_id: int | None = None):
+
+        if isinstance(node_id, int):
+            return f"{_label}_node_{node_id}"
+        
+        if isinstance(node_id, list | tuple | np.ndarray):
+            if len(node_id) == 2:
+                return f"{_label}_nodes_{int(node_id[0])}_{int(node_id[1])}"
+
+        if isinstance(element_id, int):
+            return f"{_label}_element_{element_id}"
+
+        if isinstance(line_id, int):
+            return f"{_label}_line_{line_id}"
+        
+        return ""
+
+    def line_edit_reset(self, lineEdit : QLineEdit):
+        lineEdit.clear()
+        lineEdit.setFocus()
 
     @abstractmethod
     def selection_callback(self):
