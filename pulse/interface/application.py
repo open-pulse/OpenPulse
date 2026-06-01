58ce5aaeafefa32d65abfac5178f7cbfc9a00954
André Fernandes <fpf.andre@gmail.com>
2026-06-01 17:56:52 -0300

    Reorder imports to make the splash screen appear earlier

--- pulse/interface/application.py
+++ pulse/interface/application.py
@@ -1,13 +1,10 @@
 from PySide6.QtCore import Signal
-from PySide6.QtWidgets import QApplication
 from PySide6.QtGui import QFontDatabase
+from PySide6.QtWidgets import QApplication
 
 from pulse import FONT_DIR
-from pulse.interface.main_window import MainWindow
 from pulse.interface.others.splash_screen import SplashScreen
 
-from pulse.project.config import Config
-from pulse.project.project import Project
 
 class Application(QApplication):
     selection_changed = Signal()
@@ -23,12 +20,19 @@ class Application(QApplication):
         self.processEvents()
 
         # global params
+        from pulse.project.config import Config
+
         self.config = Config()
+
+        from pulse.project.project import Project
+
         self.project = Project()
 
         # temporary solution
         self.project.initialize_pulse_file_and_loader()
 
         # gui
+        from pulse.interface.main_window import MainWindow
+
         self.main_window = MainWindow()
         self.main_window.configure_window()
