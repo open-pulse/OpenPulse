bb4b65283abf50ec0ae5433e5652e43be17fe562
vitorslongo <v.v.slongo@gmail.com>
2026-06-11 17:10:44 -0300

    adds RigidElement class and integrates mesh constraints in the mesh generation process

--- pulse/editor/structures/rigid_element.py
+++ pulse/editor/structures/rigid_element.py
@@ -1,17 +1,33 @@
 
 
 from pulse.editor.structures.linear_structure import LinearStructure
+from pulse.editor.structures.point import Point
 
 
 class RigidElement(LinearStructure):
     def __init__(self, start, end, *args, **kwargs):
         super().__init__(start, end, *args, **kwargs)
+
+    def add_to_gmsh(self, cad, convert_unit):
+        line_tags = super().add_to_gmsh(cad, convert_unit)
+        self._gmsh_line_tags = line_tags
+        return line_tags
     
     def as_vtk(self):
         from pulse.interface.viewer_3d.actors import RigidElementActor
         return RigidElementActor(self)
 
     def define_gmsh_mesh_constraints(self):
-        pass
+        import gmsh
+        for tag in self._gmsh_line_tags:
+            gmsh.model.mesh.setTransfiniteCurve(tag, 2)
+
+    @classmethod
+    def load_from_data(cls, data: dict) -> "RigidElement":
+        start = Point(*data["start_coords"])
+        end = Point(*data["end_coords"])
+        structure = cls(start, end)
+        structure.extra_info["structural_element_type"] = "rigid_element"
+        return structure
 
     
