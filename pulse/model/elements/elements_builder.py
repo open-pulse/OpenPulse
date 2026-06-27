f6f126ff1547f244aa1fa53b441059efb5bc1957
JacsonGilVargas <jgvargasemc@gmail.com>
2026-06-27 16:10:32 -0300

    Create a file containing function to build the acoustic and structural elements

--- pulse/model/elements/elements_builder.py
+++ pulse/model/elements/elements_builder.py
@@ -7,6 +7,10 @@ from pulse.model.elements.rigid_structural_element import RigidStructuralElement
 from pulse.model.elements.expansion_joint_structural_element import ExpansionJointStructuralElement
 from pulse.model.elements.valve_structural_element import ValveStructuralElement
 
+from pulse.model.elements.acoustic.acoustic_element import AcousticElement
+from pulse.model.elements.acoustic.fetm_acoustic_element import FETMAcousticElement
+from pulse.model.elements.acoustic.fem_acoustic_element import FEMAcousticElement
+
 
 def build_structural_element(element_attributes: ElementAttributes) -> StructuralElement:
 
@@ -22,16 +26,10 @@ def build_structural_element(element_attributes: ElementAttributes) -> Structura
         case "valve":
             return ValveStructuralElement(element_attributes)
 
-def build_acoustic_element(element_attributes: ElementAttributes) -> StructuralElement:
+def build_acoustic_element(element_attributes: ElementAttributes) -> AcousticElement:
 
-    match element_attributes.acoustic_element_type:
-        case "pipe_1":
-            return PipeStructuralElement(element_attributes)
-        case "beam_1":
-            return BeamStructuralElement(element_attributes)
-        case "rigid_element":
-            return RigidStructuralElement(element_attributes)
-        case "expansion_joint":
-            return ExpansionJointStructuralElement(element_attributes)
-        case "valve":
-            return ValveStructuralElement(element_attributes)
+    match element_attributes.acoustic_element_formulation:
+        case "FETM":
+            return FETMAcousticElement(element_attributes)
+        case "FEM":
+            return FEMAcousticElement(element_attributes)
