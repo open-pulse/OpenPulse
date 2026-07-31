27e47cda924fd4df696b41a58d3c1d7027598679
R0drigoSchwartz <rodrigo32003@gmail.com>
2026-07-31 11:08:18 -0300

    Updates the icon colors

--- pulse/__init__.py
+++ pulse/__init__.py
@@ -24,7 +24,7 @@ FONT_DIR = OPEN_PULSE_DIR / "interface/data/fonts/"
 USER_PATH = Path().home()
 TEMP_PROJECT_DIR = USER_PATH / "temp_pulse"
 
-LIGHT_ICON_COLOR = Color("#0069D0")
+LIGHT_ICON_COLOR = Color("#0051A2")
 DARK_ICON_COLOR = Color("#84AAFF")
 
 def app() -> "Application":
